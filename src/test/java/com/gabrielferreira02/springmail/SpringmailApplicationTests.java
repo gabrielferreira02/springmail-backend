2c1280b39120e2e9fafd3483e5fa6e3b3883b5d0
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-01-30 09:14:03 -0300

    upload base project

---
+++ src/test/java/com/gabrielferreira02/springmail/SpringmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira02.springmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
