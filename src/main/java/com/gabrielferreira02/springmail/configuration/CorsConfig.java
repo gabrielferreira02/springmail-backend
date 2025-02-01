b80860bed8df819a4d805fa27185e55313ceccc9
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-01 20:42:25 -0300

    Created cors configuration

---
+++ src/main/java/com/gabrielferreira02/springmail/configuration/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.gabrielferreira02.springmail.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
