9c96c013b4564657498b4be7e6f4cbcd67fbf224
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-11 19:28:38 -0300

    Create and update - Created swagger configuration class - Added swagger dependency in pom.xml

---
+++ src/main/java/com/gabrielferreira02/springmail/configuration/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.gabrielferreira02.springmail.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.http.HttpHeaders;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Simplified email platform api",
+                description = "This API provides a simplified email system with features for sending new emails, adding a conversation to favorites, searching conversations by username",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Gabriel Ferreira",
+                        url = "https://github.com/gabrielferreira02",
+                        email = "gabrielf.04.2002@gmail.com"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "DEV SERVER",
+                        url = "http://localhost:8080"
+                )
+        },
+        security = @SecurityRequirement(name = "Security Token")
+)
+@SecurityScheme(
+        name = "Security Token",
+        description = "Access token for api",
+        type = SecuritySchemeType.HTTP,
+        paramName = HttpHeaders.AUTHORIZATION,
+        in = SecuritySchemeIn.HEADER,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
